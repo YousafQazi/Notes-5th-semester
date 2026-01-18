dd2cda5e1925d34cd1d3b24ded5282e916d9f8ef
Yousaf Qazi <yousafqazi04@gmail.com>
2026-01-18 16:39:38 +0500

    Delete IS Lab Assignment 1 directory

--- IS Lab Assignment 1/IS Lab Assignmnt 1 Task 1.py
+++
@@ -1,8 +0,0 @@
-list1 = input("ENTER THE ELEMENTS OF FIRST LIST : ").split()
-list2 = input("ENTER THE ELEMENTS OF SECOND LIST : ").split()
-list1 = [int(a) for a in list1]
-list2 = [int(a) for a in list2]
-merged = list1 + list2
-merged.sort()
-print("Merged and Sorted List: ", merged)
-
